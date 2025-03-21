2ec95fa7443f9052d80eb7cd17b9cdb8c3b89465
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-03-20 21:30:54 -0300

    ad

--- src/main/java/fei/relogio_logico_lamport/RelogioLogicoLampertController.java
+++ src/main/java/fei/relogio_logico_lamport/RelogioLogicoLampertController.java
@@ -1,5 +1,6 @@
 package fei.relogio_logico_lamport;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,27 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 @RequestMapping("/clock")
 public class RelogioLogicoLampertController {
     
     private final AtomicInteger relogio = new AtomicInteger(0);
+    private final Random random = new Random();
 
     @GetMapping("/time")
     public int getTime() {
         return relogio.get();
     }
-
     @PostMapping("/increment")
-    public int incrementrelogio() {
-        return relogio.incrementAndGet();
+    public int incrementRelogio() {
+        int tempoPassado = random.nextInt(10) + 1;
+        int novoRelogio = relogio.addAndGet(tempoPassado);
+        System.out.println("Relógio incrementado por: " + tempoPassado + " - Novo valor: " + novoRelogio);
+        return novoRelogio;
     }
 
     @PostMapping("/message")
-    public int receiveMessage(@RequestParam int senderrelogio) {
-        int updatedrelogio = Math.max(relogio.get(), senderrelogio) + 1;
-        relogio.set(updatedrelogio);
-        return updatedrelogio;
+    public int receiveMessage(@RequestParam int senderRelogio) {
+        int updatedRelogio = Math.max(relogio.get(), senderRelogio) + 1;
+        relogio.set(updatedRelogio);
+        System.out.println("Mensagem recebida! Relógio corrigido para: " + updatedRelogio);
+        return updatedRelogio;
     }
 }
